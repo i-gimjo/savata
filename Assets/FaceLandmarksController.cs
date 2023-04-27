58bd77f718ad6e593317158e5fe6e80a6edce3ff
choye-sudo <inbrain144@gmail.com>
2023-04-27 20:20:57 +0900

    landmark model HTTP (1/6)
    AWS EC2 instance에 올려둔 docker image의 app.py와 HTTP 통신하는 C# script FaceLandmarksController을 작성하였습니다.
    
    한번에 commit 할 수 있는 용량이 정해져있어서, 나누어서 commit 합니다(1/6)

---
+++ Assets/FaceLandmarksController.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Newtonsoft.Json.Linq;
+
+public class FaceLandmarksController : MonoBehaviour
+{
+    private string url = "http://172.31.44.86:5000";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Load test image
+        Texture2D tex = Resources.Load<Texture2D>("iu_face");
+
+        // Convert Texture2D to byte array
+        byte[] bytes = tex.EncodeToPNG();
+
+        // Send put request to server
+        StartCoroutine(Put(bytes));
+    }
+
+    private bool IsValidImage(byte[] imageData)
+    {
+        // Check if image data is null or empty
+        if (imageData == null || imageData.Length == 0)
+        {
+            Debug.LogError("Invalid image data: null or empty");
+            return false;
+        }
+
+        // Check if image data can be decoded to a texture
+        Texture2D tex = new Texture2D(2, 2);
+        if (!tex.LoadImage(imageData))
+        {
+            Debug.LogError("Invalid image data: cannot be decoded to a texture");
+            return false;
+        }
+
+        return true;
+    }
+
+    IEnumerator Put(byte[] imageBytes)
+    {
+        if (!IsValidImage(imageBytes))
+        {
+            yield break;
+        }
+
+        // Create UnityWebRequest object and set method to PUT
+        UnityWebRequest request = UnityWebRequest.Put(url, imageBytes);
+        request.method = "PUT";
+
+        request.SetRequestHeader("Content-Type", "application/octet-stream");
+        request.SetRequestHeader("Accept", "application/json");
+
+        // Send the request and wait for the response
+        yield return request.SendWebRequest();
+
+        // Check for errors
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError(request.error);
+            yield break;
+        }
+
+        // Parse JSON response
+        JObject response = JObject.Parse(request.downloadHandler.text);
+        JArray landmarksArray = (JArray)response["landmarks"];
+
+        // Convert JArray to 2D float array
+        float[,] landmarks = new float[landmarksArray.Count, 2];
+        for (int i = 0; i < landmarksArray.Count; i++)
+        {
+            landmarks[i, 0] = (float)landmarksArray[i][0];
+            landmarks[i, 1] = (float)landmarksArray[i][1];
+        }
+
+        // Print landmarks to console
+        for (int i = 0; i < landmarks.GetLength(0); i++)
+        {
+            Debug.Log("Landmark " + i + ": (" + landmarks[i, 0] + ", " + landmarks[i, 1] + ")");
+        }
+    }
+}
